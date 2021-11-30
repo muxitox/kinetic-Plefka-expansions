b48cb4b1bf89ffb8ac03063e2f4283e5cdf4a0ee
muxitox <angel.poc.lopez@gmail.com>
2021-11-30 15:30:57 +0100

    Adding changes to the structure of the hidden kinetifc ising model, modify a gitignore

--- kinetic_ising/hidden_kinetic_ising.py
+++ kinetic_ising/hidden_kinetic_ising.py
@@ -32,9 +32,10 @@ class HiddenIsing:  # Asymmetric Ising model simulation class with hidden activi
 
         self.b = np.zeros(self.b_size)  # Fields
 
-        visible_units = np.ones(self.size)
-        hidden_idx = random.sample(range(0, self.ising.size), self.hidden_size)
-        visible_units[hidden_idx] = 0
+        # visible_units = np.ones(self.size)
+        # self.hidden_idx = random.sample(range(0, self.ising.size), self.hidden_size)
+        self.visible_idx = random.sample(range(0, self.ising.size), self.visible_size)
+        # visible_units[hidden_idx] = 0
 
     def random_fields(self):  # Set random values for H
         self.H = np.random.rand(self.size) * 2 - 1
@@ -47,59 +48,65 @@ class HiddenIsing:  # Asymmetric Ising model simulation class with hidden activi
 
     def sim_fit(self):
 
+        # Number of time steps in the simulation
         T = 100
-        # Produce the data:
+        # Simulate the full Kinetic Ising model to produce data
+        full_s = []
         s = []
-        for t in range(0,T):
+        for t in range(0, T):
             self.ising.ParallelUpdate()
-            s.append(self.ising.s)
+            full_s.append(self.ising.s)
+            s.append(self.ising.s[self.visible_idx])
 
+        # Initialize variables for learning
         eta = 0.1
-        LderJ_accum = 0
-        LderK_accum = 0
-        LderL_accum = 0
-
         rep = 0
         max_reps = 200
         error_lim = 0.0001
         error = np.inf
-        while error > error_lim:
-            for t in range(0,T):
-                if t != 0:
+        # Learning loop
+        while error > error_lim and rep < max_reps:
+            # Initialize the gradients to 0
+            LdJ = 0
+            LdK = 0
+            LdL = 0
 
-                    # Compute the derivative of the Likelihood wrt J
-                    self.b = np.tanh(np.dot(self.K, s[t-1]) + np.dot(self.L*b_p))
-                    tanh_h = np.tanh(self.b + np.dot(self.J, s[t-1]))
-                    sub_s_h = s[t] - tanh_h
+            # We start in index 1 because we dont have s_{t-1} for t=0
+            for t in range(1, T):
 
-                    LderJ_accum += np.dot(sub_s_h, s[t-1])
+                # Compute the derivative of the Likelihood wrt J
+                self.b = np.tanh(np.dot(self.K, s[t-1]) + np.dot(self.L*b_p))
+                tanh_h = np.tanh(self.b + np.dot(self.J, s[t-1]))
+                sub_s_h = s[t] - tanh_h
 
-                    # Compute the derivatives of the Likelihood wrt L and K
-                    if t == 1:
-                        # We need the derivative of b wrt K and L, and that'd require s_{t-2}
-                        # Which does not exist at this time step
-                        bderK_p = 0
-                        bderL_p = 0
-                    else:
-                        sub_b_1_sq = 1 - b_p ** 2
-                        bderK_p = np.dot(s[t-2] + np.dot(self.L, bderK_p2), sub_b_1_sq)
-                        bderL_p = np.dot(s[t-2] + np.dot(self.L, bderL_p2), sub_b_1_sq)
+                LdJ += np.dot(sub_s_h, s[t-1])
 
-                    sub_b_sq = 1 - self.b**2
-                    LderK_accum += np.dot(np.dot(sub_s_h, (s[t-1] + np.dot(self.L, bderK_p))), sub_b_sq)
-                    LderL_accum += np.dot(np.dot(sub_s_h, (b_p + np.dot(self.L, bderL_p))), sub_b_sq)
+                # Compute the derivatives of the Likelihood wrt L and K
+                if t == 1:
+                    # We need the derivative of b wrt K and L, and that'd require s_{t-2}
+                    # Which does not exist at this time step
+                    bdK_p = 0
+                    bdL_p = 0
+                else:
+                    sub_b_1_sq = 1 - b_p ** 2
+                    bdK_p = np.dot(s[t-2] + np.dot(self.L, bdK_p2), sub_b_1_sq)
+                    bdL_p = np.dot(s[t-2] + np.dot(self.L, bdL_p2), sub_b_1_sq)
 
-                b_p = copy.deepcopy(self.b)
-                bderK_p2 = copy.deepcopy(bderK_p)
-                bderL_p2 = copy.deepcopy(bderL_p)
+                sub_b_sq = 1 - self.b**2
+                LdK += np.dot(np.dot(sub_s_h, (s[t-1] + np.dot(self.L, bdK_p))), sub_b_sq)
+                LdL += np.dot(np.dot(sub_s_h, (b_p + np.dot(self.L, bdL_p))), sub_b_sq)
 
-            self.J = self.J + eta * LderJ_accum
-            self.K = self.K + eta * LderK_accum
-            self.L = self.L + eta * LderL_accum
+            b_p = copy.deepcopy(self.b)
+            bdK_p2 = copy.deepcopy(bdK_p)
+            bdL_p2 = copy.deepcopy(bdL_p)
+
+            self.J = self.J + eta * LdJ
+            self.K = self.K + eta * LdK
+            self.L = self.L + eta * LdL
+
+            error = max(np.max(np.abs(LdJ)), np.max(np.abs(LdK)), np.max(np.abs(LdL)))
 
             rep = rep + 1
-            if rep > max_reps:
-                break
 
 
 if __name__ == "__main__":
