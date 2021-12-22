1c0f379040190e9c2c451e7d65da9cefb65a3b2f
muxitox <angel.poc.lopez@gmail.com>
2021-12-22 17:37:13 +0100

    Add seeds for reproducibility, change temporal notation

--- hidden_kinetic_ising/hidden_kinetic_ising_it2.py
+++ hidden_kinetic_ising/hidden_kinetic_ising_it2.py
@@ -7,13 +7,12 @@ with asymmetric weights and parallel updates.
 
 import numpy as np
 from kinetic_ising import ising
-import random
 import copy
 
 
 class HiddenIsing:  # Asymmetric Ising model simulation class with hidden activity
 
-    def __init__(self, original_ising, visible_units_per, b_size=None):  # Create ising model
+    def __init__(self, original_ising, visible_units_per, b_size=None, rng=None):  # Create ising model
 
         self.ising = original_ising
         self.size = self.ising.size
@@ -32,21 +31,32 @@ class HiddenIsing:  # Asymmetric Ising model simulation class with hidden activi
         self.K = np.zeros((self.b_size, self.visible_size))  # Hidden-to-Neuron couplings
         self.L = np.zeros((self.b_size, self.b_size))  # Hidden-to-Hidden couplings
 
+        if rng:
+            self.rng = rng
+        else:
+            self.rng = np.random.default_rng()
+
         # visible_units = np.ones(self.size)
         # self.hidden_idx = random.sample(range(0, self.ising.size), self.hidden_size)
-        self.visible_idx = random.sample(range(0, self.ising.size), self.visible_size)
+        self.visible_idx = self.rng.choice(range(0, self.ising.size), self.visible_size)
         # visible_units[hidden_idx] = 0
 
     def random_wiring(self):  # Set random values for J
-        self.J = np.random.randn(self.visible_size, self.visible_size) / self.visible_size
-        self.M = np.random.randn(self.visible_size, self.b_size) / ((self.visible_size + self.b_size) / 2)
-        self.K = np.random.randn(self.b_size, self.visible_size) / ((self.visible_size + self.b_size) / 2)
-        self.L = np.random.randn(self.b_size, self.b_size) / self.b_size
+        self.J = self.rng.random((self.visible_size, self.visible_size)) / self.visible_size
+        self.M = self.rng.random((self.visible_size, self.b_size)) / ((self.visible_size + self.b_size) / 2)
+        self.K = self.rng.random((self.b_size, self.visible_size)) / ((self.visible_size + self.b_size) / 2)
+        self.L = self.rng.random((self.b_size, self.b_size)) / self.b_size
+
+        print('J', self.J)
+        print('M', self.M)
+        print('K', self.K)
+        print('L', self.L)
+        print()
 
     def sim_fit(self):
 
         # Number of time steps in the simulation
-        T = 100
+        T = 200
         # Simulate the full Kinetic Ising model to produce data
         full_s = []
         s = []
@@ -55,6 +65,7 @@ class HiddenIsing:  # Asymmetric Ising model simulation class with hidden activi
             full_s.append(self.ising.s)
             s.append(self.ising.s[self.visible_idx])
 
+        # print('Spins', s)
 
         # Initialize variables for learning
         eta = 0.1
@@ -88,69 +99,79 @@ class HiddenIsing:  # Asymmetric Ising model simulation class with hidden activi
 
                 # Compute the derivative of the Likelihood wrt J
                 tanh_h = np.tanh(np.dot(self.M, b_t1) + np.dot(self.J, s[t - 1]))
-                sub_s_h = s[t] - tanh_h
+                # print('h', np.dot(self.M, b_t1) + np.dot(self.J, s[t - 1]))
+                # print('tanh_h', tanh_h)
+                sub_s_tanhh = s[t] - tanh_h
+                # print('sub_s_tanhh', sub_s_tanhh)
 
-                LdJ += np.einsum('i,j->ij', sub_s_h, s[t - 1])
+                LdJ += np.einsum('i,j->ij', sub_s_tanhh, s[t - 1])
 
                 # Save computational load if the number of b neurons < 1
                 if self.b_size > 0:
-                    LdM += np.einsum('i,j->ij', sub_s_h, b_t1)
+                    LdM += np.einsum('i,j->ij', sub_s_tanhh, b_t1)
 
                     # Compute the Jacobians wrt K and L
                     for i in range(0, self.visible_size):
                         # Derivative of Likelihood wrt K
                         for n in range(0, self.b_size):
                             for m in range(0, self.visible_size):
-                                LdK[n, m] += sub_s_h[i] * \
-                                            (np.dot(self.M[i, :], b_t1_dK[:, n, m]))
+                                LdK[n, m] += sub_s_tanhh[i] * \
+                                            np.dot(self.M[i, :], b_t1_dK[:, n, m])
 
                         # Derivative of Likelihood wrt L
                         for n in range(0, self.b_size):
                             for m in range(0, self.b_size):
-                                LdL[n, m] += sub_s_h[i] * \
-                                            (np.dot(self.M[i, :], b_t1_dL[:, n, m]))
+                                LdL[n, m] += sub_s_tanhh[i] * \
+                                            np.dot(self.M[i, :], b_t1_dL[:, n, m])
+                                # print('LdL[i,n,m,t]', i, n, m, t, sub_s_tanhh[i] * \
+                                #             np.dot(self.M[i, :], b_t1_dL[:, n, m]))
+                                # print('LdL[n,m] accum',  n, m, t, LdL[n, m])
+                                # print('sub_s_tanhh[i]', sub_s_tanhh[i])
 
                     # Compute the necessary information for the next step
                     # At t==1, b(t-1)=0
                     b = np.tanh(np.dot(self.K, s[t - 1]) + np.dot(self.L, b_t1))
+                    # print('b', b)
 
                     # Compute the derivatives of b wrt L and K
                     # At t==1 b_t1_dK=0 and b_t1_dL=0
-                    if t != 1:
-                        for i in range(0, self.b_size):
-                            # Derivative of b wrt K
-                            for n in range(0, self.b_size):
-                                for m in range(0, self.visible_size):
-                                    b_dK[i, n, m] = (np.dot(self.L[i, :], b_t1_dK[:, n, m]))
-                                    if i == n:
-                                        b_dK[i, n, m] += s[t - 1][m]
+                    for i in range(0, self.b_size):
+                        # Derivative of b wrt K
+                        for n in range(0, self.b_size):
+                            for m in range(0, self.visible_size):
+                                b_dK[i, n, m] = np.dot(self.L[i, :], b_t1_dK[:, n, m])
+                                if i == n:
+                                    b_dK[i, n, m] += s[t - 1][m]
 
-                                    b_dK[i, n, m] *= (1 - b[i] ** 2)
+                                b_dK[i, n, m] *= (1 - b[i] ** 2)
 
-                            # Derivative of b wrt L
-                            for n in range(0, self.b_size):
-                                for m in range(0, self.b_size):
+                        # Derivative of b wrt L
+                        for n in range(0, self.b_size):
+                            for m in range(0, self.b_size):
 
-                                    b_dL[i, n, m] = (np.dot(self.L[i, :], b_t1_dL[:, n, m]))
-                                    if i == n:
-                                        b_dL[i, n, m] += b_t1[m]
+                                b_dL[i, n, m] = np.dot(self.L[i, :], b_t1_dL[:, n, m])
+                                if i == n:
+                                    b_dL[i, n, m] += b_t1[m]
 
-                                    b_dL[i, n, m] *= (1 - b[i] ** 2)
+                                b_dL[i, n, m] *= (1 - b[i] ** 2)
+                                # print('b_dL[i,n,m](t)', i, n, m, t, b_dL[i, n, m])
 
                     b_t1 = copy.deepcopy(b)
                     b_t1_dK = copy.deepcopy(b_dK)
                     b_t1_dL = copy.deepcopy(b_dL)
 
             # Normalize the gradients temporally and by the number of spins in the sum of the Likelihood
-            LdJ /= T
-            LdM /= T
-            LdK /= (self.visible_size * T)
-            LdL /= (self.visible_size * T)
+            LdJ /= (T-1)
+            LdM /= (T-1)
+            LdK /= (self.visible_size * (T-1))
+            LdL /= (self.visible_size * (T-1))
 
             # self.J = self.J + eta * LdJ
             # self.K = self.K + eta * LdK
             # self.L = self.L + eta * LdL
             # self.M = self.M + eta * LdM
+
+
 
             # Prints for debugging
             if self.b_size > 1:
@@ -158,6 +179,12 @@ class HiddenIsing:  # Asymmetric Ising model simulation class with hidden activi
             else:
                 self.L[0] = self.L[0] + eta * LdL[0]
 
+
+            # print('J', self.J)
+            # print('M', self.M)
+            # print('K', self.K)
+            # print('L', self.L)
+            # print()
 
             # Compute the error as the max component of the gradients
             # if self.b_size > 1:
@@ -186,8 +213,14 @@ class HiddenIsing:  # Asymmetric Ising model simulation class with hidden activi
 
 
 if __name__ == "__main__":
-    kinetic_ising = ising(netsize=10)
-    hidden_ising = HiddenIsing(kinetic_ising, visible_units_per=0.1, b_size=1)
+
+    # You can set up a seed here for reproducibility
+    rng = np.random.default_rng()
+
+    kinetic_ising = ising(netsize=10, rng=rng)
+    kinetic_ising.random_fields()
+    kinetic_ising.random_wiring()
+    hidden_ising = HiddenIsing(kinetic_ising, visible_units_per=0.1, b_size=1, rng=rng)
     hidden_ising.random_wiring()
 
     hidden_ising.sim_fit()
--- kinetic_ising/kinetic_ising.py
+++ kinetic_ising/kinetic_ising.py
@@ -10,27 +10,31 @@ import numpy as np
 
 class ising:              # Asymmetric Ising model simulation class
 
-    def __init__(self, netsize):  # Create ising model
+    def __init__(self, netsize, rng=None):  # Create ising model
 
         self.size = netsize                        # Network size
         self.H = np.zeros(netsize)                # Fields
         self.J = np.zeros((netsize, netsize))  # Couplings
         self.Beta = 1                            # Inverse temperature
 
+        if rng:
+            self.rng = rng
+        else:
+            self.rng = np.random.default_rng()
         self.randomize_state()                    # Set random state
 
     def randomize_state(self):        # Randomize network state
-        self.s = np.random.randint(0, 2, self.size) * 2 - 1
+        self.s = self.rng.integers(0, 2, self.size) * 2 - 1
 
     def random_fields(self):        # Set random values for H
-        self.H = np.random.rand(self.size) * 2 - 1
+        self.H = self.rng.random(self.size) * 2 - 1
 
     def random_wiring(self):        # Set random values for J
-        self.J = np.random.randn(self.size, self.size) / self.size
+        self.J = self.rng.random((self.size, self.size)) / self.size
 
     # Update the state of the network using Little parallel update rule
     def ParallelUpdate(self):
         self.h = self.H + np.dot(self.J, self.s)
-        r = np.random.rand(self.size)
+        r = self.rng.random(self.size)
         self.s = -1 + 2 * (2 * self.Beta * self.h > -
                            np.log(1 / r - 1)).astype(int)
