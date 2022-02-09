7fa4dfa74628138791b69e72dcb30bad69b14c11
muxitox <angel.poc.lopez@gmail.com>
2022-02-09 12:31:51 +0100

    Update MSE results

--- hidden_kinetic_ising/plot_MSE.py
+++ hidden_kinetic_ising/plot_MSE.py
@@ -3,29 +3,55 @@ import os
 import numpy as np
 from matplotlib import cm
 
-fig, ax = plt.subplots(3, figsize=(16, 10), dpi=100)
+fig, ax = plt.subplots(4, figsize=(16, 10), dpi=100)
 
 
 colors = []
 cmap = cm.get_cmap('plasma_r')
-for i in range(0, 6):
+for i in range(0, 5):
     colors += [cmap((i) / 4)]
 
 i = 0
 
+J_initialization = 'constantJ'
+
+
+version_folder = 'it2'
 # Set the directory you want to start from
-rootDir = './results/stronger_couplings'
+rootDir = f'./results/{version_folder}/{J_initialization}'
 for dirName, subdirList, fileList in os.walk(rootDir):
     head, num_vis = os.path.split(dirName)
-    head, num_neurons = os.path.split(head)
-    if str.isdigit(num_neurons) and str.isdigit(num_vis):
-
-
+    _, num_total_neurons = os.path.split(head)
+    if str.isdigit(num_total_neurons) and str.isdigit(num_vis):
 
         MSE_m_list = []
         MSE_D_list = []
         MSE_C_list = []
+        log_ell_list = []
         hidden_list = []
+
+        # Get it3 results
+        it3_path = f'./results/it3/{J_initialization}/{num_total_neurons}/{num_vis}'
+        it3_filelist = [f for f in os.listdir(it3_path) if os.path.isfile(os.path.join(it3_path, f))]
+
+        it3_filename = None
+        for fname in it3_filelist:
+            name, ext = os.path.splitext(fname)
+            if ext == '.npz':
+                it3_filename = fname
+
+        data = np.load(os.path.join(it3_path, it3_filename))
+        MSE_m = data['MSE_m']
+        MSE_C = data['MSE_C']
+        MSE_D = data['MSE_D']
+        log_ell = data['log_ell']
+
+        MSE_m_list.append(MSE_m)
+        MSE_C_list.append(MSE_C)
+        MSE_D_list.append(MSE_D)
+        log_ell_list.append(np.exp(log_ell))
+        hidden_list.append(-1)
+
         new_fileList = []
         for fname in fileList:
             name, ext = os.path.splitext(fname)
@@ -42,28 +68,42 @@ for dirName, subdirList, fileList in os.walk(rootDir):
                 MSE_m = data['MSE_m']
                 MSE_C = data['MSE_C']
                 MSE_D = data['MSE_D']
+                log_ell = data['log_ell']
 
                 MSE_m_list.append(MSE_m)
                 MSE_C_list.append(MSE_C)
                 MSE_D_list.append(MSE_D)
+                log_ell_list.append(np.exp(log_ell))
                 hidden_list.append(num_hidden)
 
-        ax[0].plot(hidden_list, MSE_m_list, color=colors[i], label=f'Original netsize: {num_neurons}. Visible units size: {num_vis}')
-        ax[1].plot(hidden_list, MSE_C_list, color=colors[i], label=f'Original netsize: {num_neurons}. Visible units size: {num_vis}')
-        ax[2].plot(hidden_list, MSE_D_list, color=colors[i], label=f'Original netsize: {num_neurons}. Visible units size: {num_vis}')
+        ax[0].plot(hidden_list, MSE_m_list, color=colors[i],
+                   label=f'Original netsize: {num_total_neurons}. Visible units size: {num_vis}')
+        ax[1].plot(hidden_list, MSE_C_list, color=colors[i],
+                   label=f'Original netsize: {num_total_neurons}. Visible units size: {num_vis}')
+        ax[2].plot(hidden_list, MSE_D_list, color=colors[i],
+                   label=f'Original netsize: {num_total_neurons}. Visible units size: {num_vis}')
+        ax[3].plot(hidden_list, log_ell_list, color=colors[i],
+                   label=f'Original netsize: {num_total_neurons}. Visible units size: {num_vis}')
 
         i += 1
+
 
 ax[0].set_ylabel('MSE m')
 ax[1].set_ylabel('MSE C')
 ax[2].set_ylabel('MSE D')
-ax[2].set_xlabel('Number of hidden units')
+ax[3].set_ylabel('Likelihood')
 
+ax[3].set_xlabel('Number of hidden units')
+
+# plt.suptitle('J = 1/self.size + self.rng.random((self.size, self.size)) / np.sqrt(self.size)')
+# plt.suptitle('J = self.rng.random((self.size, self.size)) / np.sqrt(self.size)')
+# plt.suptitle('J = self.rng.random((self.size, self.size)) / self.size')
+plt.suptitle('J = 1/self.size')
 
 plt.legend()
 
 # plt.show()
-plt.savefig('results/stronger_couplings/MSE')
+plt.savefig(f'results/MSE/MSE_{J_initialization}')
 
 
 
