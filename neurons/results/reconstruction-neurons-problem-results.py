17daa9411ed25c93245ce7fe8ad1cbd57681ee64
muxitox <angel.poc.lopez@gmail.com>
2022-04-22 20:50:39 +0200

    Add variable to stop harcoding neuron size

--- neurons/results/reconstruction-neurons-problem-results.py
+++ neurons/results/reconstruction-neurons-problem-results.py
@@ -18,18 +18,6 @@ plt.rc('font', **font)
 plt.rc('legend', **{'fontsize': 20})
 
 
-size = 512
-R = 1000000
-R1 = 100000
-J0 = 1.0
-H0 = 0.5
-Js = 0.1
-
-T = 2**7
-iu1 = np.triu_indices(166, 1)
-
-offset = np.arange(3)
-width = 0.20
 
 B = 51
 betas = np.linspace(0, 2, B)
@@ -67,6 +55,7 @@ for ib in range(len(betas)):
     C_final = data_r['C_final']
     D_final = data_r['D_final']
 
+    iu1 = np.triu_indices(len(m_final), 1)
 
     m_list[ib] = np.mean(m_final)
     C_list[ib] = np.mean(C_final[iu1])
@@ -92,7 +81,7 @@ pos_l = [-0.2, 1.0]
 
 letters = ['A', 'B', 'C', 'D', 'E', 'F']
 
-fig, ax = plt.subplots(1, 3, figsize=(16, 10 * 2 / 3), dpi=300)
+fig, ax = plt.subplots(1, 3, figsize=(16, 10 / 3), dpi=300)
 
 
 nrow = 0
@@ -109,7 +98,7 @@ ax[ncol].set_ylabel(
 ax[
     ncol].text(pos_l[0],
                pos_l[1],
-               r'\textbf ' + letters[2],
+               r'\textbf ' + letters[0],
                transform=ax[
                             ncol].transAxes,
                fontsize=20,
@@ -130,7 +119,7 @@ ax[ncol].set_ylabel(
 ax[
     ncol].text(pos_l[0],
                pos_l[1],
-               r'\textbf ' + letters[0],
+               r'\textbf ' + letters[1],
                transform=ax[
                             ncol].transAxes,
                fontsize=20,
@@ -150,7 +139,7 @@ ax[ncol].set_ylabel(
 ax[
     ncol].text(pos_l[0],
                pos_l[1],
-               r'\textbf ' + letters[1],
+               r'\textbf ' + letters[2],
                transform=ax[
                             ncol].transAxes,
                fontsize=20,
@@ -164,12 +153,12 @@ if not isExist:
     # Create a new directory because it does not exist
     os.makedirs(folder)
 
-plt.figlegend(
-    loc='upper center',
-    bbox_to_anchor=(
-        0.5,
-        1.),
-    borderaxespad=0,
-    ncol=5)
+# plt.figlegend(
+#     loc='upper center',
+#     bbox_to_anchor=(
+#         0.5,
+#         1.),
+#     borderaxespad=0,
+#     ncol=5)
 fig.tight_layout(h_pad=0.3, w_pad=0.7, rect=[0, 0, 1, 0.95])
 plt.savefig(f'{folder}/dataset-{dataset}-neurons-reconstruted-transition.pdf', bbox_inches='tight')
